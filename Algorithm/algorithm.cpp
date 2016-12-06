d09a2d23a8d25b057ff1d155d48cd2365a51598d
tjpassaro <tjpassaro@gmail.com>
2016-12-06 05:49:44 -0500

    Concentrations added. NOT Tested yet.

--- Algorithm/algorithm.cpp
+++ Algorithm/algorithm.cpp
@@ -18,6 +18,14 @@
 
 using namespace std;
 
+void parse_reqs(vector<vector<string> > &reqs, string &f_name, vector<string> &comments);
+void parse_reqs(vector<vector<string> > &reqs, string &f_name);
+bool special_compare(string &req, map<string, int> &classes, set<string> &unacceptable, bool noRepeat);
+int free_electives(map<string, int> &classes, string credits);
+int concentration_compare(string &concentration, map<string, int> &classes_credits);
+void compare_courses(vector<vector<string> > &reqs, map<string, int> &classes, vector<int> &needed, bool noRepeat, int num_courses);
+void compare_courses(vector<vector<string> > &reqs, map<string, int> &classes, vector<int> &needed);
+
 //Takes in a datastructure which it will add classes to
 void parse_reqs(vector<vector<string> > &reqs, string &f_name, vector<string> &comments){
 	ifstream inFile(f_name);
@@ -58,6 +66,42 @@ void parse_reqs(vector<vector<string> > &reqs, string &f_name, vector<string> &c
     	cout << "Could not open " << f_name << endl;
 }
 
+//Takes in a datastructure which it will add classes to
+void parse_reqs(vector<vector<string> > &reqs, string &f_name){
+	ifstream inFile(f_name);
+	if (inFile.is_open()){
+		string temp;
+		string most_recent_comment = "";
+    	while(getline(inFile,temp)){
+    		//If comment then update field
+		    if( temp.find("//") != -1)
+		    	continue;
+
+		    vector<string> options;
+		    //Loop through and parse
+	    	int a = temp.find("||");
+	    	while(a != -1){
+	    		string course = temp.substr(0,a);
+	    		//If it cant be taken to satisfy req
+	    		if(course.substr(0,1).compare("!") == 0)
+	    			options.insert(options.begin(), course);
+	    		else
+	    			options.push_back(course);
+	    		temp=temp.substr(a+2);
+	    		a = temp.find("||");
+	    	}
+	    	if(temp.substr(0,1).compare("!") == 0)
+	    		options.insert(options.begin(),temp);
+	    	else
+	    		options.push_back(temp);
+	    	reqs.push_back(options);
+    	}
+    	inFile.close();
+    }
+    else
+    	cout << "Could not open " << f_name << endl;
+}
+
 bool special_compare(string &req, map<string, int> &classes, set<string> &unacceptable, bool noRepeat){
 	//Iterate through set checking for a match
 	for(map<string, int>::iterator itr = classes.begin(); itr != classes.end(); ++itr){
@@ -73,38 +117,10 @@ bool special_compare(string &req, map<string, int> &classes, set<string> &unacce
 	return false;
 }
 
-bool concentration_compare(string &concentration, map<string, int> &classes_credits){
-	//Checks if repition in multiple requirements is allowed (ex Comm intensive)
-	bool canRepeat = false;
-	if(concentration.substr(0,1).compare("#") == 0){
-		concentration = concentration.substr(1);
-		canRepeat = true;
-	}
-
-	int num_courses = stoi(concentration.substr(0,1));
-	concentration = concentration.substr(2);
-	int a = concentration.find("&&");
-	string f_name;
-	if(a == -1)
-		f_name = "../Concentrations/" + concentration + ".txt";
-	else
-		f_name = "../Concentrations/" + concentration.substr(0,a) + ".txt";
-	vector<vector<string> > conc_reqs;
-	vector<int> not_taken;
-	//parse_reqs(conc_reqs, f_name, comments);
-	//  IMPLEMENT COURSE COUNTING
-	//compare_courses(conc_reqs, classes, not_taken, !canRepeat, num_courses);
-	if(conc_reqs.size()-not_taken.size() == num_courses)
-		return true;
-	return false;
-}
-
-int free_electives(map<string, int> &classes, string credits ){
+int free_electives(map<string, int> &classes, string credits){
 	int numCreds = stoi(credits);
 	int currentCreds = 0;
-	cout << "h";
 	for(map<string, int>::iterator itr = classes.begin(); itr != classes.end(); ++itr){
-		cout << currentCreds << ": " << itr->first << endl;
 		currentCreds += itr->second;
 		if(currentCreds >= numCreds)
 			return 0;
@@ -112,7 +128,27 @@ int free_electives(map<string, int> &classes, string credits ){
 	return numCreds - currentCreds;
 }
 
-void compare_courses(vector<vector<string> > &reqs, map<string, int> &classes, vector<int> &needed, bool noRepeat){
+int concentration_compare(string &concentration, map<string, int> &classes_credits){
+	//Checks if repition in multiple requirements is allowed (ex Comm intensive)
+	bool noRepeat = true;
+	if(concentration.substr(0,1).compare("#") == 0){
+		concentration = concentration.substr(1);
+		noRepeat = false;
+	}
+
+	int num_courses = stoi(concentration.substr(0,1));
+	string c_name = "Database/" + concentration.substr(2) + ".txt";
+	vector<vector<string> > conc_reqs;
+	vector<int> not_taken;
+	parse_reqs(conc_reqs, c_name);
+	// IMPLEMENT COURSE COUNTING
+	compare_courses(conc_reqs, classes_credits, not_taken, noRepeat, num_courses);
+	if(conc_reqs.size()-not_taken.size() >= num_courses)
+		return 0;
+	return conc_reqs.size()-not_taken.size();
+}
+
+void compare_courses(vector<vector<string> > &reqs, map<string, int> &classes, vector<int> &needed, bool noRepeat, int num_courses){
 	for(int i = 0; i < reqs.size(); ++i){
 		//Find unacceptable course
 		set<string> unacceptable;
@@ -151,7 +187,9 @@ void compare_courses(vector<vector<string> > &reqs, map<string, int> &classes, v
 				//Range(CSCI-4000+)
 				if(temp.substr(0,1).compare("@") == 0){
 					string name = temp.substr(1);
-					satisfied = concentration_compare(name, classes);
+					int num_left = concentration_compare(name, classes);
+					if(num_left == 0)
+						satisfied = true;
 				}
 				else if(temp.find("+") != -1){
 					satisfied = special_compare(temp, classes, unacceptable, noRepeat);
@@ -174,7 +212,92 @@ void compare_courses(vector<vector<string> > &reqs, map<string, int> &classes, v
 		}
 		if(!satisfied)
 			needed.push_back(i);
+		if(reqs.size()-needed.size() >= num_courses)
+			break;
 	}
+}
+
+void compare_courses(vector<vector<string> > &reqs, map<string, int> &classes, vector<int> &needed){
+	for(int i = 0; i < reqs.size(); ++i){
+		//Find unacceptable course
+		set<string> unacceptable;
+		int before = 0;
+		while(reqs[i][before].substr(0,1).compare("!") == 0){
+			unacceptable.insert(reqs[i][before].substr(1));
+			before++;
+		}
+		bool satisfied = false;
+		for(int j = before; j < reqs[i].size(); ++j){
+			string element = reqs[i][j];
+			if(element.find("$") == 0){
+				string credits = element.substr(1);
+				int dif = free_electives(classes, credits);
+				if(dif > 0){
+					vector<string> creds;
+					creds.push_back("You need " + to_string(dif) + " more free elective credits.");
+					reqs[i] = creds;
+				}
+				else
+					satisfied = true;
+			}
+			else if(element.find("(") != -1)
+				element = element.substr(1).substr(0,element.length()-2);
+				int index; 
+				string temp;
+			while(true){
+				satisfied = false;
+				index = element.find("&&");
+				if(index == -1)
+					temp = element;
+				else{
+					temp = element.substr(0,index);
+					element = element.substr(index+2);
+				}
+				//Range(CSCI-4000+)
+				if(temp.substr(0,1).compare("@") == 0){
+					string name = temp.substr(1);
+					int num_left = concentration_compare(name, classes);
+					if(num_left == 0)
+						satisfied = true;
+				}
+				else if(temp.find("+") != -1){
+					satisfied = special_compare(temp, classes, unacceptable, true);
+				}
+				//Regular courses
+				else{
+					if(classes.find(temp) != classes.end()){
+						satisfied = true;
+						classes.erase(reqs[i][j]);
+					}
+				}
+				if(!satisfied)
+					break;
+				if(index == -1)
+					break;
+			}
+			if(satisfied)
+				break;
+		}
+		if(!satisfied)
+			needed.push_back(i);
+	}
+}
+
+void read_classes(map<string, int> &classes, string fname){
+	ifstream inFile(fname);
+	if (inFile.is_open()){
+		string temp;
+    	while(getline(inFile,temp)){
+    		//If comment then skip
+		    if(temp.find("//") != -1)
+		    	continue;
+		    int index = temp.find(":");
+		    classes[temp.substr(0, index)] = stoi(temp.substr(index+1));
+		}
+		inFile.close();
+    }
+    else
+    	cout << "error";
 }
 
 bool file_output(vector<vector<string> > &reqs, vector<int> &needed, string f_name, vector<string> &comments){
@@ -193,23 +316,6 @@ bool file_output(vector<vector<string> > &reqs, vector<int> &needed, string f_na
 	}
 	outFile.close();
 	return true;
-}
-
-void read_classes(map<string, int> &classes, string fname){
-	ifstream inFile(fname);
-	if (inFile.is_open()){
-		string temp;
-    	while(getline(inFile,temp)){
-    		//If comment then skip
-		    if(temp.find("//") != -1)
-		    	continue;
-		    int index = temp.find(":");
-		    classes[temp.substr(0, index)] = stoi(temp.substr(index+1));
-		}
-		inFile.close();
-    }
-    else
-    	cout << "error";
 }
 
 /*&pair<map<string, string>, map<string, string> > runAlgo(string req_file, map<string, int> courses){
@@ -232,9 +338,8 @@ int main(int argc, const char* args[]){
 	map<string, int> classes;
 	read_classes(classes, classes_fname);
 	parse_reqs(reqs, input_file_name, comments);
-
 	
-	compare_courses(reqs, classes, needed, true);
+	compare_courses(reqs, classes, needed);
 	for(int i = 0; i < reqs.size(); ++i){
 		for(int j = 0; j < reqs[i].size(); ++j)
 			cout << reqs[i][j] << " ";
@@ -243,8 +348,6 @@ int main(int argc, const char* args[]){
 	cout << endl;
 	for(int i = 0; i <needed.size(); ++i)
 		cout << needed[i] << endl;
-	for(int i = 0; i < comments.size(); ++ i)
-		cout << comments[i] << endl;
 	file_output(reqs, needed, OUTPUT_FILE_NAME, comments);
 }
 
