22f483514d5addac16e29e988130353b23ee5bfb
tjpassaro <tjpassaro@gmail.com>
2016-12-06 14:23:03 -0500

    Added documentation to some functions.

--- Algorithm.cpp
+++ Algorithm.cpp
@@ -1,11 +1,8 @@
 //Thomas Passaro
 
 /*To do:
-	- Dynamic memory for reqs vector and dictionary??
-	- Implement credits counting
-	- Pointers
-	- Output
-	- Compare
+	- Documentation
+	- User Friendly output
 */
 
 
@@ -113,13 +110,17 @@ void parse_reqs(vector<vector<string> > &reqs, string &f_name){
     	cout << "Could not open " << f_name << endl;
 }
 
+//Takes a class range such as CSCI-4000+ and checks to see if any class satisfies that requirement
+//Accounts for any classes that do not count in unacceptable
 bool special_compare(string &req, map<string, int> &classes, set<string> &unacceptable, bool noRepeat){
 	//Iterate through set checking for a match
 	for(map<string, int>::iterator itr = classes.begin(); itr != classes.end(); ++itr){
 		int courseNum = stoi(itr->first.substr(5,4));
 		int reqNum = stoi(req.substr(5,4));
 		bool isUnacceptable = false;
+		//If the course subjects match (ex CSCI) and the course number is greater than the required level and it is not unacceptable
 		if(itr->first.substr(0,4).compare(req.substr(0,4)) == 0 && courseNum >= reqNum  && unacceptable.find(itr->first) == unacceptable.end()){
+			//Erases the class if it cannot be double counted
 			if(noRepeat)
 				classes.erase(itr);
 			return true;
@@ -310,6 +311,9 @@ void compare_courses(vector<vector<string> > &reqs, map<string, int> &classes, v
 	}
 }
 
+//Read classes is only used in main() for modular testing
+//Reads in a text file with one course per line
+//Users enter classes in the GUI and this is not present in a deployment
 void read_classes(map<string, int> &classes, string fname){
 	ifstream inFile(fname);
 	if (inFile.is_open()){
@@ -319,6 +323,7 @@ void read_classes(map<string, int> &classes, string fname){
 		    if(temp.find("//") != -1)
 		    	continue;
 		    int index = temp.find(":");
+		    //Adds each class with the course name as the key and number of credits as the value
 		    classes[temp.substr(0, index)] = stoi(temp.substr(index+1));
 		}
 		inFile.close();
@@ -327,33 +332,44 @@ void read_classes(map<string, int> &classes, string fname){
     	cout << "error";
 }
 
+//Only used for modular testing and is called solely in main()
+//Outputs the data to a file 
 void file_output(string f_name, pair<map<string, string>*, map<string, string>* > pairMH){
 	ofstream outFile(f_name);
   	if (!outFile.is_open())
     	cout << "Error writing to output file";
     outFile << "CAUTION: Read the READ.ME before using this program." << endl;
     outFile << "It contains extremely vital information about the current state of the program and it's known weaknesses/shortcomings." << endl << endl;
-	//Loop through in needed and output each req
+	//Iterate through in needed and output each unsatisfied major req
 	outFile << "Major requirements to complete: " << endl;
 	for(map<string,string>::iterator itr = pairMH.first->begin(); itr != pairMH.first->end(); ++itr)
 		outFile << itr->second << endl << itr->first << endl;
+	//HASS
 	outFile << endl << "HASS requirements to complete: "  << endl;
 	for(map<string,string>::iterator itr = pairMH.second->begin(); itr != pairMH.second->end(); ++itr)
 		outFile << itr->second << endl << itr->first << endl;
 	outFile.close();
 }
 
+//This is called by the GUI.
+//Returns a pair of maps where each map contains key-value pairs of an unsatisfied requirement and a comment about the requirement
+//The first element in the pair is a map with Major requirements in it, while the second is for HASS
 pair<map<string, string>*, map<string, string>* > runAlgo(string req_file, map<string, int> courses){
+	//Initializes all hass data structures
 	map<string, string>* hass = new map<string, string>();
 	vector<vector<string> > hass_reqs;
 	vector<int> hass_needed;
 	vector<string> hass_comments;
+	//Initializes all major data structures
 	map<string, string>* major = new map<string, string>();
 	vector<vector<string> > major_reqs;
 	vector<int> major_needed;
 	vector<string> major_comments;
+	//Parses the requirements file and puts the requirements and comments in their corresponding data structures
 	parse_reqs(req_file, major_reqs, major_comments, hass_reqs, hass_comments);
+	//Compares the necessary requirements for hass with the user's completed classes and fills the missing requirements in hass_needed
 	compare_courses(hass_reqs, courses, hass_needed);
+	//Assigns each missing req with its comment in the hass map
 	for(int i = 0; i < hass_needed.size(); ++i){
 		string temp = "";
 		vector<string> components = hass_reqs[hass_needed[i]];
@@ -361,7 +377,9 @@ pair<map<string, string>*, map<string, string>* > runAlgo(string req_file, map<s
 			temp = temp + components[j] + " ";
 		(*hass)[temp.substr(0,temp.length()-1)]= hass_comments[hass_needed[i]];
 	}
+	//Compares the requirements for major 
 	compare_courses(major_reqs, courses, major_needed);
+	//Generates major map
 	for(int i = 0; i < major_needed.size(); ++i){
 		string temp = "";
 		vector<string> components = major_reqs[major_needed[i]];
@@ -369,16 +387,20 @@ pair<map<string, string>*, map<string, string>* > runAlgo(string req_file, map<s
 			temp = temp + components[j] + " ";
 		(*major)[temp.substr(0,temp.length()-1)] = major_comments[major_needed[i]];
 	}
+	//Returns the pair of maps.
 	return make_pair(major,hass);
 }
 
+//int main is used for modular testing of the algorithm itself
+//Will not be present in any deployments
+//It calls the runAlgo function like the gui does and takes command line arguments and outputs to a file
+#if 0
 int main(int argc, const char* args[]){
 	if(args[1]==NULL || args[2]==NULL){
 		cerr << "One or more arguments is NULL." << endl;
 		return 0;
 	}
 
-	/*vector<vector<string> > reqs;
 	vector<string> comments;
 	vector<int> needed;*/
 	string input_file_name= args[1];
@@ -387,17 +409,7 @@ int main(int argc, const char* args[]){
 	map<string, int> classes;
 	read_classes(classes, classes_fname);
 	pair<map<string, string>*, map<string, string>* > pairMH = runAlgo(input_file_name, classes);
-	//parse_reqs(reqs, input_file_name, comments);
 	
-	//compare_courses(reqs, classes, needed);
-	/*for(int i = 0; i < reqs.size(); ++i){
-		for(int j = 0; j < reqs[i].size(); ++j)
-			cout << reqs[i][j] << " ";
-		cout << endl;
-	}
-	cout << endl;
-	for(int i = 0; i <needed.size(); ++i)
-		cout << needed[i] << endl;*/
 	file_output(OUTPUT_FILE_NAME, pairMH);
 }
-
+#endif
