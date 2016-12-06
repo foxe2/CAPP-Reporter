4d11b4ccbb47fa58109cd93a7d9137634f62cb0b
tjpassaro <tjpassaro@gmail.com>
2016-12-06 18:45:44 -0500

    Added some constants and standardized variable names

--- Algorithm.cpp
+++ Algorithm.cpp
@@ -15,20 +15,21 @@
 
 using namespace std;
 
-void parse_reqs(string &f_name, vector<vector<string> > &major_reqs, vector<string> &major_comments, vector<vector<string> > &hass_reqs, vector<string> &hass_comments);
-void parse_reqs(vector<vector<string> > &reqs, string &f_name);
-bool special_compare(string &req, map<string, int> &classes, set<string> &unacceptable, bool noRepeat);
-int free_electives(map<string, int> &classes, string credits);
-int concentration_compare(string &concentration, map<string, int> &classes_credits);
-void compare_courses(vector<vector<string> > &reqs, map<string, int> &classes, vector<int> &needed, bool noRepeat, int num_courses);
+void parse_reqs(const string &fName, vector<vector<string> > &majorReqs, vector<string> &majorComments, vector<vector<string> > &hassReqs, vector<string> &hassComments);
+void parse_reqs(vector<vector<string> > &reqs, const string &fName);
+bool special_compare(const string &req, map<string, int> &classes, const set<string> &unacceptable, bool noRepeat);
+int free_electives(map<string, int> &classes, const string &credits);
+int concentration_compare(const string &initConcentration, map<string, int> &classes_credits);
+void compare_courses(vector<vector<string> > &reqs, map<string, int> &classes, vector<int> &needed, bool noRepeat, int numCourses);
 void compare_courses(vector<vector<string> > &reqs, map<string, int> &classes, vector<int> &needed);
+pair<map<string, string>*, map<string, string>* > runAlgo(const string &req_file, map<string, int> courses);
 
 //Takes in a datastructure which it will add classes to
-void parse_reqs(string &f_name, vector<vector<string> > &major_reqs, vector<string> &major_comments, vector<vector<string> > &hass_reqs, vector<string> &hass_comments){
-	ifstream inFile(f_name);
+void parse_reqs(const string &fName, vector<vector<string> > &majorReqs, vector<string> &majorComments, vector<vector<string> > &hassReqs, vector<string> &hassComments){
+	ifstream inFile(fName);
 	if (inFile.is_open()){
 		string temp;
-		string most_recent_comment = "";
+		string mostRecentComment = "";
     	bool inHass = false;
     	while(getline(inFile,temp)){
     		if(temp.find("// HASS Requirement") != -1){
@@ -36,9 +37,9 @@ void parse_reqs(string &f_name, vector<vector<string> > &major_reqs, vector<stri
     			continue;
     		}
     		//If comment then update field
-    		int comment_index = temp.find("//");
-		    if( comment_index != -1){
-		    	most_recent_comment = temp.substr(comment_index+2);
+    		int commentIndex = temp.find("//");
+		    if( commentIndex != -1){
+		    	mostRecentComment = temp.substr(commentIndex+2);
 		    	continue;
 		    }
 
@@ -60,26 +61,26 @@ void parse_reqs(string &f_name, vector<vector<string> > &major_reqs, vector<stri
 	    	else
 	    		options.push_back(temp);
 	    	if(inHass && options[0].find("$") == -1){
-	    		hass_reqs.push_back(options);
-	    		hass_comments.push_back(most_recent_comment);
+	    		hassReqs.push_back(options);
+	    		hassComments.push_back(mostRecentComment);
 	    	}
 	    	else{
-	    		major_reqs.push_back(options);
-	    		major_comments.push_back(most_recent_comment);
+	    		majorReqs.push_back(options);
+	    		majorComments.push_back(mostRecentComment);
 	    	}
     	}
     	inFile.close();
     }
     else
-    	cout << "Could not open " << f_name << endl;
+    	cout << "Could not open " << fName << endl;
 }
 
 //Takes in a datastructure which it will add classes to
-void parse_reqs(vector<vector<string> > &reqs, string &f_name){
-	ifstream inFile(f_name);
+void parse_reqs(vector<vector<string> > &reqs, const string &fName){
+	ifstream inFile(fName);
 	if (inFile.is_open()){
 		string temp;
-		string most_recent_comment = "";
+		string mostRecentComment = "";
     	while(getline(inFile,temp)){
     		//If comment then update field
 		    if( temp.find("//") != -1)
@@ -107,12 +108,12 @@ void parse_reqs(vector<vector<string> > &reqs, string &f_name){
     	inFile.close();
     }
     else
-    	cout << "Could not open " << f_name << endl;
+    	cout << "Could not open " << fName << endl;
 }
 
 //Takes a class range such as CSCI-4000+ and checks to see if any class satisfies that requirement
 //Accounts for any classes that do not count in unacceptable
-bool special_compare(string &req, map<string, int> &classes, set<string> &unacceptable, bool noRepeat){
+bool special_compare(const string &req, map<string, int> &classes, const set<string> &unacceptable, bool noRepeat){
 	//Iterate through set checking for a match
 	for(map<string, int>::iterator itr = classes.begin(); itr != classes.end(); ++itr){
 		int courseNum = stoi(itr->first.substr(5,4));
@@ -129,7 +130,7 @@ bool special_compare(string &req, map<string, int> &classes, set<string> &unacce
 	return false;
 }
 
-int free_electives(map<string, int> &classes, string credits){
+int free_electives(map<string, int> &classes, const string &credits){
 	int numCreds = stoi(credits);
 	int currentCreds = 0;
 	for(map<string, int>::iterator itr = classes.begin(); itr != classes.end(); ++itr){
@@ -140,40 +141,40 @@ int free_electives(map<string, int> &classes, string credits){
 	return numCreds - currentCreds;
 }
 
-int concentration_compare(string &concentration, map<string, int> &classes_credits){
+int concentration_compare(const string &initConcentration, map<string, int> &classes_credits){
 	//Checks if repition in multiple requirements is allowed (ex Comm intensive)
+	string concentration = initConcentration;
 	bool noRepeat = true;
 	if(concentration.substr(0,1).compare("#") == 0){
 		concentration = concentration.substr(1);
 		noRepeat = false;
 	}
 	cout << concentration << endl;
-	int num_courses = stoi(concentration.substr(0,1));
-	cout << num_courses <<endl;
-	string c_name = "Database/Concentrations/" + concentration.substr(2) + ".txt";
+	int numCourses = stoi(concentration.substr(0,1));
+	cout << numCourses <<endl;
+	string cName = "Database/Concentrations/" + concentration.substr(2) + ".txt";
 	vector<vector<string> > conc_reqs;
-	vector<int> not_taken;
-	parse_reqs(conc_reqs, c_name);
+	vector<int> notTaken;
+	parse_reqs(conc_reqs, cName);
 	// IMPLEMENT COURSE COUNTING
-	compare_courses(conc_reqs, classes_credits, not_taken, noRepeat, num_courses);
+	compare_courses(conc_reqs, classes_credits, notTaken, noRepeat, numCourses);
 	for(int i = 0; i < conc_reqs.size(); ++i){
 		for(int j = 0; j < conc_reqs[i].size(); ++j)
 			cout << conc_reqs[i][j] << " ";
 		cout << endl;
 	}
 	cout<< "NEEDED: " << endl;
-	for(int i = 0; i < not_taken.size(); ++i)
-		cout<< not_taken[i];
+	for(int i = 0; i < notTaken.size(); ++i)
+		cout<< notTaken[i];
 	cout << endl<<"done"<<endl;
-	if(conc_reqs.size()-not_taken.size() >= num_courses)
+	if(conc_reqs.size()-notTaken.size() >= numCourses)
 		return -1;
-	cout << "res: " << conc_reqs.size()-not_taken.size()<<endl;
-	return conc_reqs.size()-not_taken.size();
+	cout << "res: " << conc_reqs.size()-notTaken.size()<<endl;
+	return conc_reqs.size()-notTaken.size();
 }
 
-void compare_courses(vector<vector<string> > &reqs, map<string, int> &classes, vector<int> &needed, bool noRepeat, int num_courses){
+void compare_courses(vector<vector<string> > &reqs, map<string, int> &classes, vector<int> &needed, bool noRepeat, int numCourses){
 	cout << "reqs size:  " << reqs.size()<<endl;
-	vector<int> classes_to_delete;
 	for(int i = 0; i < reqs.size(); ++i){
 		//Find unacceptable course
 		set<string> unacceptable;
@@ -237,9 +238,9 @@ void compare_courses(vector<vector<string> > &reqs, map<string, int> &classes, v
 		}
 		if(!satisfied)
 			needed.push_back(i);
-		if(i-needed.size()+1 >= num_courses){
+		if(i-needed.size()+1 >= numCourses){
 			cout<< needed.size()<< endl;
-			cout << num_courses<<"i: "<<i<<endl;
+			cout << numCourses<<"i: "<<i<<endl;
 			break;
 		}
 	}
@@ -334,8 +335,8 @@ void read_classes(map<string, int> &classes, string fname){
 
 //Only used for modular testing and is called solely in main()
 //Outputs the data to a file 
-void file_output(string f_name, pair<map<string, string>*, map<string, string>* > pairMH){
-	ofstream outFile(f_name);
+void file_output(string fName, pair<map<string, string>*, map<string, string>* > pairMH){
+	ofstream outFile(fName);
   	if (!outFile.is_open())
     	cout << "Error writing to output file";
     outFile << "CAUTION: Read the READ.ME before using this program." << endl;
@@ -354,38 +355,38 @@ void file_output(string f_name, pair<map<string, string>*, map<string, string>* 
 //This is called by the GUI.
 //Returns a pair of maps where each map contains key-value pairs of an unsatisfied requirement and a comment about the requirement
 //The first element in the pair is a map with Major requirements in it, while the second is for HASS
-pair<map<string, string>*, map<string, string>* > runAlgo(string req_file, map<string, int> courses){
+pair<map<string, string>*, map<string, string>* > runAlgo(const string &req_file, map<string, int> courses){
 	//Initializes all hass data structures
 	map<string, string>* hass = new map<string, string>();
-	vector<vector<string> > hass_reqs;
-	vector<int> hass_needed;
-	vector<string> hass_comments;
+	vector<vector<string> > hassReqs;
+	vector<int> hassNeeded;
+	vector<string> hassComments;
 	//Initializes all major data structures
 	map<string, string>* major = new map<string, string>();
-	vector<vector<string> > major_reqs;
-	vector<int> major_needed;
-	vector<string> major_comments;
+	vector<vector<string> > majorReqs;
+	vector<int> majorNeeded;
+	vector<string> majorComments;
 	//Parses the requirements file and puts the requirements and comments in their corresponding data structures
-	parse_reqs(req_file, major_reqs, major_comments, hass_reqs, hass_comments);
-	//Compares the necessary requirements for hass with the user's completed classes and fills the missing requirements in hass_needed
-	compare_courses(hass_reqs, courses, hass_needed);
+	parse_reqs(req_file, majorReqs, majorComments, hassReqs, hassComments);
+	//Compares the necessary requirements for hass with the user's completed classes and fills the missing requirements in hassNeeded
+	compare_courses(hassReqs, courses, hassNeeded);
 	//Assigns each missing req with its comment in the hass map
-	for(int i = 0; i < hass_needed.size(); ++i){
+	for(int i = 0; i < hassNeeded.size(); ++i){
 		string temp = "";
-		vector<string> components = hass_reqs[hass_needed[i]];
+		vector<string> components = hassReqs[hassNeeded[i]];
 		for(int j = 0; j < components.size(); ++j)
 			temp = temp + components[j] + " ";
-		(*hass)[temp.substr(0,temp.length()-1)]= hass_comments[hass_needed[i]];
+		(*hass)[temp.substr(0,temp.length()-1)]= hassComments[hassNeeded[i]];
 	}
 	//Compares the requirements for major 
-	compare_courses(major_reqs, courses, major_needed);
+	compare_courses(majorReqs, courses, majorNeeded);
 	//Generates major map
-	for(int i = 0; i < major_needed.size(); ++i){
+	for(int i = 0; i < majorNeeded.size(); ++i){
 		string temp = "";
-		vector<string> components = major_reqs[major_needed[i]];
+		vector<string> components = majorReqs[majorNeeded[i]];
 		for(int j = 0; j < components.size(); ++j)
 			temp = temp + components[j] + " ";
-		(*major)[temp.substr(0,temp.length()-1)] = major_comments[major_needed[i]];
+		(*major)[temp.substr(0,temp.length()-1)] = majorComments[majorNeeded[i]];
 	}
 	//Returns the pair of maps.
 	return make_pair(major,hass);
@@ -394,15 +395,15 @@ pair<map<string, string>*, map<string, string>* > runAlgo(string req_file, map<s
 //int main is used for modular testing of the algorithm itself
 //Will not be present in any deployments
 //It calls the runAlgo function like the gui does and takes command line arguments and outputs to a file
-#if 0
+
 int main(int argc, const char* args[]){
-	if(args[1]==NULL || args[2]==NULL){
+	/*if(args[1]==NULL || args[2]==NULL){
 		cerr << "One or more arguments is NULL." << endl;
 		return 0;
 	}
 
 	vector<string> comments;
-	vector<int> needed;*/
+	vector<int> needed;
 	string input_file_name= args[1];
 	string classes_fname = args[2];
 	string OUTPUT_FILE_NAME = "Test_Files/algorithm_output.txt";
@@ -410,6 +411,6 @@ int main(int argc, const char* args[]){
 	read_classes(classes, classes_fname);
 	pair<map<string, string>*, map<string, string>* > pairMH = runAlgo(input_file_name, classes);
 	
-	file_output(OUTPUT_FILE_NAME, pairMH);
+	file_output(OUTPUT_FILE_NAME, pairMH);*/
 }
-#endif
+
