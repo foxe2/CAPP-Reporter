1f139cf58dbda04c51b3ba571d83a5b945007ab7
zwimer <zwimer@gmail.com>
2016-12-06 23:15:28 -0500

    Added std to Algorithm.hpp

--- Algorithm.hpp
+++ Algorithm.hpp
@@ -5,21 +5,21 @@
 #include <map>
 
 //For ease of reading
-typedef map<string, int> courseMap;
-typedef vector<string> commentVector;
-typedef map<string, string> outputMap;
-typedef vector<vector<string> > reqsVector;
+typedef std::map<std::string, int> courseMap;
+typedef std::vector<std::string> commentVector;
+typedef std::map<std::string, std::string> outputMap;
+typedef std::vector<std::vector<std::string> > reqsVector;
                                            
 //Forward declarations
-void parse_reqs(const string &fName, reqsVector& majorReqs, commentVector& 
+void parse_reqs(const std::string &fName, reqsVector& majorReqs, commentVector& 
                 majorComments, reqsVector& hassReqs, commentVector& hassComments);
-void parse_reqs(reqsVector& reqs, const string &fName);
-bool special_compare(const string &req, courseMap& classes, 
-                        const set<string> &unacceptable, bool noRepeat);
-int free_electives(courseMap& classes, const string &credits);
-int concentration_compare(const string &initConcentration, courseMap& classes_credits);
+void parse_reqs(reqsVector& reqs, const std::string &fName);
+bool special_compare(const std::string &req, courseMap& classes, 
+                        const std::set<std::string> &unacceptable, bool noRepeat);
+int free_electives(courseMap& classes, const std::string &credits);
+int concentration_compare(const std::string &initConcentration, courseMap& classes_credits);
 void compare_courses(reqsVector& reqs, courseMap& classes, 
-                        vector<int> &needed, bool noRepeat, int numCourses);
-void compare_courses(reqsVector& reqs, courseMap& classes, vector<int> &needed);
-pair<outputMap*, outputMap* > runAlgo(const string &req_file, courseMap courses);
+                        std::vector<int> &needed, bool noRepeat, int numCourses);
+void compare_courses(reqsVector& reqs, courseMap& classes, std::vector<int> &needed);
+pair<outputMap*, outputMap* > runAlgo(const std::string &req_file, courseMap courses);
 
